d2bcbc4a1bb119bbd3d8b46cf603218a229ab1bf
Dachi Papashvili <dachi123.dp@gmail.com>
2023-03-28 23:58:08 +0400

    Update initial commit

--- index.js
+++ index.js
@@ -5,58 +5,134 @@ const searchMovies = document.getElementById("search-movies")
 const myWatchlist = document.getElementById("watchlist")
 const mainDiv = document.getElementById("main")
 const addToWatchlist = document.getElementById("add")
+const removeSelectedMovie = document.getElementById("remove")
 const container =  document.getElementById("container")
+const selectedMovies = document.getElementById("selected")
 
 
-searchBtn.addEventListener("click", ()=> { 
-    fetch("https://www.omdbapi.com/?s=&apikey=51f90224")
-    .then(res=>res.json())
-    .then(data=>{
-        renderMovie()
+searchBtn.addEventListener("click", (e)=> { 
+        e.preventDefault();
+        renderMovies()
         mainDiv.classList.remove("hidden")
-
 })
 
-})
 
-function renderMovie(){  
-  fetch(`https://www.omdbapi.com/?t=${searchTitle.value}&apikey=51f90224`)
-  .then(res=>res.json())
-  .then(data=>{
+function renderMovies() {
+  fetch(`https://www.omdbapi.com/?s=${searchTitle.value}&apikey=51f90224`)
+  .then(res => res.json())
+  .then(data => {
     console.log(data)
-     main.innerHTML = `
-   <div class="poster"><img class="poster" src="${data.Poster}"></div>
-   <div class="tiitle"><h3>${data.Title}</h3></div>
-   <div class="rating"<h4>${data.imdbRating}</h4></div>
-   <div class="duration"><h4>${data.Runtime}</h4></div>
-   <div class="genre"><h4>${data.Genre}</h4></div>
-   <div class="add" id="add" onclick="watchList()"><h4>Add To Watchlist</h4></div>
-   <div class="plot"<p>${data.Plot}</p></div>
-   `
-})
+     data.Search.forEach(data => {
+         const movieName = (data.Title).split(' ').join('+')
+              fetch(`https://www.omdbapi.com/?t=${movieName}&apikey=51f90224`)
+                  .then(res => res.json())
+                  .then(data => {
+                      main.innerHTML += `
+              <div class="first-div">
+              <img class="poster" src="${data.Poster}">
+              <div class="second-div">
+              <div class="title">
+              <h3 class="movie-title">${data.Title}</h3>
+              <h4 class="rating">⭐${data.imdbRating}</h4>
+              </div> 
+              <div class="length">
+              <h4 class="duration">${data.Runtime}</h4>
+              <h4 class="genre">${data.Genre}</h4>
+              <h4 class="add" id="add" onclick="watchList(event)"><i class="fa-solid fa-circle-plus"></i>Watchlist</h4>
+              </div>
+              <p class="plot">${data.Plot}</p>
+              </div>
+              </div>
+            `;
+          });
+       });
+       
+    });
+          
+          
+    searchTitle.value = "";                              
+    main.innerHTML = "";
+  
 }
 
 
-let movieArray = []
+ let movieArray = []
 
 
-    searchMovies.addEventListener("click", function(){
+    searchMovies.addEventListener("click", () =>{
         myWatchlist.classList.remove("hidden")
         searchMovies.classList.add("hidden")
-        document.getElementById("container").classList.remove("hidden")
-
+        container.classList.remove("hidden")
         
-
 })
 
-    myWatchlist.addEventListener("click", function(){
+   myWatchlist.addEventListener("click", () =>{
+        myWatchlist.classList.remove("hidden")
         searchMovies.classList.remove("hidden")
-        myWatchlist.classList.add("hidden")
-
+        main.classList.add("hidden")
+        selectedMovies.classList.remove("hidden")
+        searchMovies.classList.add("hidden")
 })
 
 
+  
+function watchList(event) {
+  
+  const firstDiv = event.target.closest('.first-div');
 
-function watchList(){
-  movieArray.push(renderMovie())
+  const movieTitle = firstDiv.getElementsByClassName('movie-title')[0].textContent;
+  const movieRating = firstDiv.getElementsByClassName('rating')[0].textContent;
+  const movieRuntime = firstDiv.getElementsByClassName('duration')[0].textContent;
+  const movieGenre = firstDiv.getElementsByClassName('genre')[0].textContent;
+  const moviePlot = firstDiv.getElementsByClassName('plot')[0].textContent;
+  const moviePoster = firstDiv.getElementsByClassName('poster')[0].src;
+
+
+  let movie = { title: movieTitle, rating: movieRating, runtime: movieRuntime, genre: movieGenre, plot: moviePlot, poster: moviePoster };
+
+
+  console.log('movie', movie)
+
+  movieArray.push(movie);
+  localStorage.setItem('watchlist', JSON.stringify(movieArray));
+  JSON.parse(localStorage.getItem('watchlist'));
+       selectedMovies.innerHTML += `
+     <div class="movie-div" id="movie-div">
+    <img class="poster" src="${movie.poster}" alt="${movie.title}">
+    <div class="second-div">
+    <div class="title">
+    <h3 class="movie-title">${movie.title}</h3>
+    <h4 class="rating">${movie.rating}</h4>
+     </div>
+     <div class="length">
+    <h4 class="duration">${movie.runtime}</h4>
+    <h4 class="genre">${movie.genre}</h4>
+    <h4 class="remove" id="remove" onclick="removeMovie(event)"><i class="fa-solid fa-circle-minus"></i>Remove</h4>
+    </div>
+    <p class="plot">plot ${movie.plot}</p>
+    </div>
+  </div>
+`;
+    selectedMovies.classList.add("hidden")
+  
+  }
+  
+  
+  // Delete selected movie in the watchlist
+  function removeMovie(event) {
+  const movieDiv = event.target.closest('.movie-div');
+  const movieTitle = movieDiv.getElementsByClassName('movie-title')[0].textContent;
+
+  // Find the index of the selected movie in the movieArray
+  const index = movieArray.findIndex(movie => movie.title === movieTitle);
+  if (index >= 0) {
+    // Remove the selected movie from the movieArray
+    movieArray.splice(index, 1);
+
+    // Update the localStorage with the updated movieArray
+    localStorage.setItem('watchlist', JSON.stringify(movieArray));
+
+    // Remove the selected movie from the selectedMovies element
+     movieDiv.remove();
+  }
 }
